36d0687642ae3d830aa0ec6a45b91fcc9a230504
the-ahuja-lab <60312173+the-ahuja-lab@users.noreply.github.com>
2021-03-27 09:14:48 +0530

    Add files via upload

---
+++ main/Dependencies.R
@@ -0,0 +1,102 @@
+install.packages("BiocManager")
+install.packages("shiny")
+install.packages("Seurat")
+install.packages("ggplot2")
+install.packages("shinythemes")
+install.packages("shinyalert")
+install.packages("shinyFiles")
+install.packages("shinyjs")
+install.packages("shinybusy")
+install.packages("shinyWidgets")
+install.packages("shinyalert")
+install.packages("shinyFiles")
+install.packages("shinyBS")
+install.packages("igraph")
+install.packages("shinyalert")
+install.packages("textshape")
+install.packages("tidyr")
+install.packages("tidyverse")
+install.packages("reshape2")
+install.packages("data.table")
+install.packages("networkD3")
+install.packages("ggplot2")
+install.packages("rtracklayer")
+install.packages("dplyr")
+install.packages("cli")
+install.packages("impute")
+install.packages("memisc")
+install.packages(devtools)
+library(devtools)
+install_github("debsin/dropClust", dependencies = T)
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("TissueEnrich")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("SummarizedExperiment")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("SingleCellExperiment")
+
+BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("org.Hs.eg.db")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("karyoploteR")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("dorothea")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("bcellViper")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("viper")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("AUCell")
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+# 
+ BiocManager::install("DESeq2")
+# 
+ if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("MAST")
+# 
+# BiocManager::install("TissueEnrich")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("TissueEnrich")
+# 
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("TissueEnrich")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("TissueEnrich")
+# 
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("TissueEnrich")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("TissueEnrich")
+# 
